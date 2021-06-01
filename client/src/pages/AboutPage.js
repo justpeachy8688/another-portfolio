92b3dead4a1897028a59b6e7b98e4548da8ac839
Taylor Sanders <tsanders3515@gmail.com>
2021-06-01 12:46:13 -0800

    about page font size + centering img

--- client/src/pages/AboutPage.js
+++ client/src/pages/AboutPage.js
@@ -2,6 +2,7 @@ import React from 'react'
 import Hero from "../components/Hero";
 import Content from "../components/Content";
 import Row from "react-bootstrap/Row";
+import Col from "react-bootstrap/Col";
 
 import about from "../assets/images/paris.png";
 
@@ -12,20 +13,23 @@ function AboutPage(props) {
         <div>
             <Hero title={props.title} />
             <Content>
-                <Row className="">
-                    {/* <img className="about-image" src={about} alt="paris" /> */}
+                <Row className="m-auto">
+                    <div className="container justify-content-center">
+                        <img className="mx-auto about-image" src={about} alt="paris" />
+                    </div>
                 </Row>
-                <Row>
-                    Full Stack Developer who studied at the University of Washington. I currently live in Anchorage, Alaska.
-                    As I've grown as a developer, I have worked alongside many talented developers and designers who have
-                    raised my standards for what's expected of any application.
-                    Through these experiences, I've had the opportunity to create memorable products that are not only
-                    enjoyable to use but
-                    are written in code that's maintainable and easy to understand.
+                    <Row className="about-text">
+                        Full Stack Developer who studied at the University of Washington. I currently live in Anchorage, Alaska.
+                        As I've grown as a developer, I have worked alongside many talented developers and designers who have
+                        raised my standards for what's expected of any application.
+                        Through these experiences, I've had the opportunity to create memorable products that are not only
+                        enjoyable to use but
+                        are written in code that's maintainable and easy to understand.
+                        
                 </Row>
-            </Content>
+            </Content >
 
-        </div>
+        </div >
     )
 }
 
