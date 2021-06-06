b32def8853e2b17b1725e68e5d8bbac5288cb7ea
Taylor Sanders <tsanders3515@gmail.com>
2021-06-06 13:49:45 -0800

    font and background color

---
+++ build/precache-manifest.478513e8af64599c4cdf64ab6b3d0c32.js
@@ -0,0 +1,58 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5bf124f9af5443bdc5815bb6c3e0f731",
+    "url": "/another-portfolio/index.html"
+  },
+  {
+    "revision": "beda233ba4066b0d654d",
+    "url": "/another-portfolio/static/css/2.3a0c7bd3.chunk.css"
+  },
+  {
+    "revision": "5104686e12685c5008ca",
+    "url": "/another-portfolio/static/css/main.4bf51efb.chunk.css"
+  },
+  {
+    "revision": "beda233ba4066b0d654d",
+    "url": "/another-portfolio/static/js/2.0b36b9c8.chunk.js"
+  },
+  {
+    "revision": "d6b29286b0bbae6e17db84419d44fb6a",
+    "url": "/another-portfolio/static/js/2.0b36b9c8.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5104686e12685c5008ca",
+    "url": "/another-portfolio/static/js/main.d42b1c0f.chunk.js"
+  },
+  {
+    "revision": "359adc0f7837ca24650c",
+    "url": "/another-portfolio/static/js/runtime-main.d6dba6d9.js"
+  },
+  {
+    "revision": "237d407cf437d3d949b351694f0fa795",
+    "url": "/another-portfolio/static/media/Weather.237d407c.png"
+  },
+  {
+    "revision": "de6aa7c4e3d4b88713ca746ef3576e36",
+    "url": "/another-portfolio/static/media/envision.de6aa7c4.png"
+  },
+  {
+    "revision": "8035044156a3870c858d6f3dc6c2c713",
+    "url": "/another-portfolio/static/media/lets+Venture.80350441.png"
+  },
+  {
+    "revision": "2e25075753a1e1265be0e041136debe8",
+    "url": "/another-portfolio/static/media/paris.2e250757.png"
+  },
+  {
+    "revision": "930dae6b9aba3f2536bacc98da87ced1",
+    "url": "/another-portfolio/static/media/scheduler.930dae6b.png"
+  },
+  {
+    "revision": "c9581bc984eee0c12cbbd084930baa74",
+    "url": "/another-portfolio/static/media/team-mate.c9581bc9.png"
+  },
+  {
+    "revision": "a0c5bafbb554874f167e0ae85bb85488",
+    "url": "/another-portfolio/static/media/watchNext-pic2.a0c5bafb.png"
+  }
+]);
